5397fbde2f28430a6bbf564919ec6ba89de5f835
Matias Kyriacou <matiaskyriacou@gmail.com>
2025-08-15 20:37:30 +0300

    Update RegisterToNote.cpp

--- RegisterToNote.cpp
+++ RegisterToNote.cpp
@@ -1,31 +1,27 @@
 #include "RegisterToNote.h"
 
-const byte register1Values[EWI_NOTERANGE] = {
-  72, 72, 78, 70, 78, 78, 78, 78, 79, 79, 71, 95, 93, 63, 127, 222, 255, 247
-};
-
-const byte register2Values[EWI_NOTERANGE] = {
-  16, 48, 48, 48, 112, 96, 120, 122, 120, 122, 122, 122, 122, 122, 122, 122, 122, 122
+const int noteButtonMap[EWI_BUTTONS] = {
+  11,10,9,8,7,6,5,4,3,2,1,0,23
 };
 
 
 const byte notePitches[EWI_NOTERANGE] = {
-  PitchToNote::B3b_,  // A#
-  PitchToNote::B3_,   // B
-  PitchToNote::C4_,   // C
-  PitchToNote::C4s_,  // C# (alias for D4b_)
-  PitchToNote::D4_,   // D
-  PitchToNote::D4s_,  // D# (alias for E4b_)
-  PitchToNote::E4_,   // E
-  PitchToNote::F4_,   // F
-  PitchToNote::F4s_,  // F#
-  PitchToNote::G4_,   // G
-  PitchToNote::G4s_,  // G#
-  PitchToNote::A4_,   // A
-  PitchToNote::B4b_,  // A# (alt)
-  PitchToNote::B4b_,  // A# (alt 2)
-  PitchToNote::B4_,   // B (alt)
-  PitchToNote::C5_,   // C (alt)
-  PitchToNote::C5s_,  // C# (alt) (alias for D5b_)
-  PitchToNote::D5_,   // D (alt)
+  PitchToNote::B3b_,  // A#3
+  PitchToNote::B3_,   // B3
+  PitchToNote::C4_,   // C4
+  PitchToNote::C4s_,  // C#4
+  PitchToNote::D4_,   // D4
+  PitchToNote::D4s_,  // D#4
+  PitchToNote::E4_,   // E4
+  PitchToNote::F4_,   // F4
+  PitchToNote::F4s_,  // F#4
+  PitchToNote::G4_,   // G4
+  PitchToNote::G4s_,  // G#4
+  PitchToNote::A4_,   // A4
+  PitchToNote::B4b_,  // A#4
+  PitchToNote::B4b_,  // A#4 (alt)
+  PitchToNote::B4_,   // B4
+  PitchToNote::C5_,   // C5
+  PitchToNote::C5s_,  // C#5
+  PitchToNote::D5_    // D5
 };
