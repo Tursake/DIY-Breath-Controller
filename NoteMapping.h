d7b75d32744e00fca0a46d04033986452e99d8fb
Matias Kyriacou <matiaskyriacou@gmail.com>
2025-08-15 20:37:15 +0300

    Update NoteMapping.h

--- NoteMapping.h
+++ NoteMapping.h
@@ -6,21 +6,57 @@
 #include "RegisterToNote.h"
 #include "Config.h"
 
+const boolean noteCombinations[EWI_NOTERANGE][EWI_BUTTONS] = {
+  {1,0,1,1,0,1,0,1,1,1,0,1,1},  // A#
+  {1,0,1,1,0,1,0,1,1,1,0,0,1},  // B
+  {1,0,1,1,0,0,0,1,1,1,0,0,1},  // C
+  {1,0,1,1,1,0,0,1,1,1,0,0,1},  // C#
+  {1,0,1,1,0,0,0,1,1,1,0,0,0},  // D
+  {1,0,1,1,0,0,0,1,1,1,1,0,0},  // D#
+  {1,0,1,1,0,0,0,1,1,0,0,0,0},  // E
+  {1,0,1,1,0,0,0,1,0,0,0,0,0},  // F
+  {1,0,1,1,0,0,0,0,1,0,0,0,0},  // F#
+  {1,0,1,1,0,0,0,0,0,0,0,0,0},  // G
+  {1,0,1,1,1,0,0,0,0,0,0,0,0},  // G#
+  {1,0,1,0,0,0,0,0,0,0,0,0,0},  // A
+  {1,0,1,0,0,0,1,0,0,0,0,0,0},  // A#
+  {1,1,0,0,0,0,0,0,0,0,0,0,0},  // A# alt
+  {1,0,0,0,0,0,0,0,0,0,0,0,0},  // B
+  {0,0,1,0,0,0,0,0,0,0,0,0,0},  // C
+  {0,0,0,0,0,0,0,0,0,0,0,0,0},  // C#
+  {0,0,0,0,1,0,0,0,0,0,0,0,0}   // D
+};
+
 // Returns the note index for the given register states, or -1 if no match
-static inline int getNoteIndex(uint8_t reg1, uint8_t reg2) {
+static inline int getNoteIndex(boolean* touchStates) {
   for (int i = 0; i < EWI_NOTERANGE; i++) {
-    if (reg1 == register1Values[i] && reg2 == register2Values[i]) {
-      return i;
+    bool match = true;
+    for (int j = 0; j < EWI_BUTTONS; j++) {
+      if (touchStates[noteButtonMap[j]] != noteCombinations[i][j]) {
+        match = false;
+        break;
+      }
     }
+    if (match) return i; // index into notePitches
   }
   return -1;
 }
 
-// Returns the MIDI note number for given register values, or -1 if no match
-static inline int getMIDINoteForRegisters(uint8_t reg1, uint8_t reg2) {
-  int idx = getNoteIndex(reg1, reg2);
-  if (idx == -1) return -1;
-  return notePitches[idx];
+// Returns the *index* into notePitches[] for given registers
+static inline int getNoteIndexForRegisters(boolean* touchStates) {
+  return getNoteIndex(touchStates); // just return index
 }
+
+
+// Returns true if this note has no buttons pressed (blow-only)
+bool isBlowOnly(int noteIndex) {
+    for (int i = 0; i < EWI_BUTTONS; i++) {
+        if (noteCombinations[noteIndex][i]) return false;
+    }
+    return true;
+}
+
+
+
 
 #endif
