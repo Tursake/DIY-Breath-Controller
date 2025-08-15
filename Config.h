e3581f7641e28cf2e3822c00fa19e38afadb9738
Matias Kyriacou <matiaskyriacou@gmail.com>
2025-08-15 20:37:01 +0300

    Update Config.h

--- Config.h
+++ Config.h
@@ -6,17 +6,6 @@
 // ==== NOTE RANGE ====
 constexpr uint8_t EWI_NOTERANGE = 18;
 
-// ==== SHIFT REGISTER PINS ====
-constexpr uint8_t CLOCK_ENABLE_PIN1 = 4;
-constexpr uint8_t DATA_IN1          = 5;
-constexpr uint8_t LOAD1             = 7;
-
-constexpr uint8_t CLOCK_ENABLE_PIN2 = 8;
-constexpr uint8_t DATA_IN2          = 9;
-constexpr uint8_t LOAD2             = 11;
-
-constexpr uint8_t CLOCK_IN          = 6; // Shared clock pin
-
 // ==== BREATH SENSOR ====
 constexpr uint8_t PSI_PIN           = 1;
 constexpr int PSI_AMBIENT_RAW       = 370;
@@ -29,5 +18,13 @@ constexpr uint8_t MIDI_BREATH_CC    = 11;
 
 // ==== TOUCH SENSOR ====
 constexpr uint8_t TOUCH_SENSOR_ADDR = 0x08;
+
+// ==== TOUCH BUTTONS ====
+constexpr uint8_t EWI_BUTTONS = 13;
+constexpr uint8_t MPR121_1 = 0x5A;
+constexpr uint8_t MPR121_2 = 0x5B;
+constexpr uint8_t TOUCH_BYTES = 2;
+constexpr int MPR121_irqpin_1 = 8;
+constexpr int MPR121_irqpin_2 = 7;
 
 #endif
