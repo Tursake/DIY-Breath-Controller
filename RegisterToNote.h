55bd562c158e2541677958af56813f150a16c051
Matias Kyriacou <matiaskyriacou@gmail.com>
2025-08-15 20:37:37 +0300

    Update RegisterToNote.h

--- RegisterToNote.h
+++ RegisterToNote.h
@@ -5,8 +5,7 @@
 #include "Config.h"
 
 // Declare arrays as extern here (no initializer!)
-extern const byte register1Values[EWI_NOTERANGE];
-extern const byte register2Values[EWI_NOTERANGE];
+extern const int noteButtonMap[EWI_BUTTONS];
 extern const byte notePitches[EWI_NOTERANGE];
 
 #endif
